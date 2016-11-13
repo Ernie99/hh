e30c79e9b35875dee8b955c3817b24861996cd3f
Ernie <ernie@ernietech.com>
2016-11-13 14:19:02 -0500

    rename files for routing section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {HeroesComponent} from './heroes.component';
+
 import {HeroDetailComponent} from "./hero-detail.component";
+import {AppComponent} from "./app.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
